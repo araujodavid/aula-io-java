d37f8a69cc87a1a153940fe4935008aa31fe3813
araujodavid <david.brasillo@gmail.com>
2016-06-13 11:08:15 -0300

    Add README and gitignore

---
+++ src/main/java/br/com/arida/App.java
@@ -0,0 +1,13 @@
+package br.com.arida;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
